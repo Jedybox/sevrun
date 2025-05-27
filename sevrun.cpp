e11d23815a73f7de1dd2375059b9c46d4a7425f3
codingbox <96364834+Jedybox@users.noreply.github.com>
2025-05-27 18:43:18 +0800

    first commit

---
+++ sevrun.cpp
@@ -0,0 +1,25 @@
+﻿// sevrun.cpp : Defines the entry point for the application.
+//
+
+#include "sevrun.h"
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2) {
+		std::cerr << "Error: no valid argument" << std::endl;
+		return 1;
+	}
+
+	for (const auto& arg : std::vector<std::string>(argv + 1, argv + argc)) {
+		if (IsValidFlag(arg) || IsValidCommand(arg)) continue;
+		else {
+			std::cerr << "Error: Invalid argument '" << arg << "'" << std::endl;
+			return 1;
+		}
+	}
+
+	return 0;
+}
---
+++ sevrun.h
@@ -0,0 +1,34 @@
+﻿// sevrun.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#ifndef FLAGS_H
+#define FLAGS_H
+const std::vector<std::string> flags = { "-s","-v",};
+#endif
+
+#ifndef COMMANDS_H
+#define COMMANDS_H
+const std::vector<std::string> commands = { "help", "serve", "add", "rename", "redi", "remove", "list"};
+#endif
+
+#ifndef ISVALID_COMMAND_FUNC
+#define ISVALID_COMMAND_FUNC
+bool IsValidCommand(const std::string& command) {
+	for (const auto& cmd : commands) if (cmd == command) return true;
+	return false;
+}
+#endif
+
+#ifndef ISVALID_FLAG
+#define ISVALID_FLAG
+bool IsValidFlag(const std::string& flag) {
+	for (const auto& f : flags) if (f == flag) return true;
+	return false;
+}
+#endif
